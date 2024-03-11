4e6f8e6edef43409ef56cda02d19189f53466882
CaueAnjos <kawidosanjos@outlook.com>
2024-03-11 18:02:55 -0300

    Create LifeComponent API

--- Source/Pangaea/LifeComponent.h
+++ Source/Pangaea/LifeComponent.h
@@ -12,17 +12,49 @@ class PANGAEA_API ULifeComponent : public UActorComponent
 public:	
 	ULifeComponent();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LifeStatus", meta = (ClampMin = "0"))
-	float MaxLife;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LifeStatus", meta = (ClampMin = "0")
-	float Defense;
-
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(Replicated, EditAnywhere, Category = "LifeStatus", meta = (ClampMin = "0"))
+	float _MaxHealth;
+
+	UPROPERTY(Replicated, EditAnywhere, Category = "LifeStatus", meta = (ClampMin = "0"))
+	float _Defense;
 
 public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION(BlueprintCallable)
+	void SetMaxHealth(float value);
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE float GetMaxHealth() const
+	{
+		return _MaxHealth;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void SetDefense(float value);
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE float GetDefense() const
+	{
+		return _Defense;
+	}
+
+	UFUNCTION(BlueprintCallable)
+	void Damage(float damageAmount);
+
+	UFUNCTION(BlueprintCallable)
+	void Cure(float cureAmount);
+
+private:	
+	UPROPERTY(ReplicatedUsing=Rep_CurrentHealth)
+	float _CurrentHealth;
+
+	UFUNCTION()
+	void Rep_CurrentHealth();
+
 };
