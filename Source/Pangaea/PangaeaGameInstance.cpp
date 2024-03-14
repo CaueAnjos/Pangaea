2faf351202ed8817c2f961cd010b9557fec6b599
CaueAnjos <kawidosanjos@outlook.com>
2024-03-14 19:14:10 -0300

    add NetWork Functions to Start or Join a game session on GameInstance

--- Source/Pangaea/PangaeaGameInstance.cpp
+++ Source/Pangaea/PangaeaGameInstance.cpp
@@ -1,2 +1,20 @@
 #include "PangaeaGameInstance.h"
+#include "Kismet/GameplayStatics.h"
 
+void UPangaeaGameInstance::StartListenServer()
+{
+	UWorld* world = GEngine->GetCurrentPlayWorld();
+	UGameplayStatics::OpenLevel(world, TEXT("TopDownMap"), true, TEXT("?listen"));
+}
+
+void UPangaeaGameInstance::JoinAsClient(FString IPAdress)
+{
+	UWorld* world = GEngine->GetCurrentPlayWorld();
+	UGameplayStatics::OpenLevel(world, TEXT("TopDownMap"), true, TEXT("?join"));
+}
+
+void UPangaeaGameInstance::LeaveGame()
+{
+	UWorld* world = GEngine->GetCurrentPlayWorld();
+	UGameplayStatics::OpenLevel(world, TEXT("LobbyMap"));
+}
--- Source/Pangaea/PangaeaGameInstance.h
+++ Source/Pangaea/PangaeaGameInstance.h
@@ -8,5 +8,14 @@ UCLASS()
 class PANGAEA_API UPangaeaGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+public:	
+	UFUNCTION(BlueprintCallable, Category = "NetWork")
+	void StartListenServer();
 	
+	UFUNCTION(BlueprintCallable, Category = "NetWork")
+	void JoinAsClient(FString IPAdress);
+
+	UFUNCTION(BlueprintCallable, Category = "NetWork")
+	void LeaveGame();
+
 };
