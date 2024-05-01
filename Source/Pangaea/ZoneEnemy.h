c7cf2a6878dcd8e7e80008bce67e16f0888334cd
CaueAnjos <kawidosanjos@outlook.com>
2024-05-01 12:00:08 -0300

    Implement ZoneEnemy

--- Source/Pangaea/ZoneEnemy.h
+++ Source/Pangaea/ZoneEnemy.h
@@ -2,9 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "UObject/Interface.h"
+#include "AmbushZone.h"
 #include "ZoneEnemy.generated.h"
 
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, Blueprintable)
 class UZoneEnemy : public UInterface
 {
 	GENERATED_BODY()
@@ -15,4 +16,9 @@ class PANGAEA_API IZoneEnemy
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Ambush")
+	void OnStartAmbush(AAmbushZone* Ambush);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Ambush")
+	void OnStopAmbush(AAmbushZone* Ambush);
 };
